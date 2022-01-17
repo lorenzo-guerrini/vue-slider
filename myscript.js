c416e3d273dde635bce9cd84215aac4a9c4aee30
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-01-17 02:55:43 +0100

    Esercizio finito senza bonus

--- myscript.js
+++ myscript.js
@@ -1,110 +1,50 @@
 let app = new Vue({
     el: "#myApp",
     data: {
-        imagesArray: [
-            'img/01.jpg',
-            'img/02.jpg',
-            'img/03.jpg',
-            'img/04.jpg',
-            'img/05.jpg'
+        images: [
+            {
+                item: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                item: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.'
+            },
+            {
+                item: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+            },
+            {
+                item: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            },
+            {
+                item: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+            }
         ],
         selected: 0
     },
     methods: {
-        isSelected: function(index) {
-            return (index == this.selected)?"selected":"unselected";
+        isSelected: function (index) {
+            return (index == this.selected) ? "selected" : "unselected";
+        },
+        increasePointer: function () {
+            this.selected++;
+            if (this.selected > this.images.length - 1) {
+                this.selected = 0;
+            }
+        },
+        decreasePointer: function () {
+            this.selected--;
+            if (this.selected < 0) {
+                this.selected = this.images.length - 1;
+            }
         }
     }
-})
-
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-];
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
-
-// Imposta immagine, titolo e descrizione iniziali a sx
-document.querySelector(".showing-image").style.backgroundImage = "url(" + items[0] + ")";
-
-document.querySelector(".showing-image-title").innerHTML = title[0];
-
-document.querySelector(".showing-image-text").innerHTML = text[0];
-
-//Contatore item
-let currentItemIndex = 0;
-
-// Funzionamento arrow up
-document.querySelector(".arrow.up").addEventListener("click", function () {
-
-    //Gestisce l'item da deselezionare a dx
-    const oldItem = document.querySelector(".selector-image-" + currentItemIndex);
-
-    oldItem.classList.remove("selected");
-    oldItem.classList.add("unselected");
-
-    //Gestisce il contatore diminuendolo
-    currentItemIndex--;
-    if (currentItemIndex < 0) {
-        currentItemIndex = items.length - 1;
-    }
-
-    //Gestisce l'item da selezionare a dx
-    const newItem = document.querySelector(".selector-image-" + currentItemIndex);
-
-    newItem.classList.remove("unselected");
-    newItem.classList.add("selected");
-
-    //Gestisce il comportamento degli elementi a sx
-    document.querySelector(".showing-image").style.backgroundImage = "url(" + items[currentItemIndex] + ")";
-
-    document.querySelector(".showing-image-title").innerHTML = title[currentItemIndex];
-
-    document.querySelector(".showing-image-text").innerHTML = text[currentItemIndex];
-});
-
-// Funzionamento arrow down
-document.querySelector(".arrow.down").addEventListener("click", function () {
-
-    //Gestisce l'item da deselezionare a dx
-    const oldItem = document.querySelector(".selector-image-" + currentItemIndex);
-
-    oldItem.classList.remove("selected");
-    oldItem.classList.add("unselected");
-
-    //Gestisce il contatore aumentandolo
-    currentItemIndex++;
-    if (currentItemIndex > items.length - 1) {
-        currentItemIndex = 0;
-    }
-
-    //Gestisce l'item da selezionare a dx
-    const newItem = document.querySelector(".selector-image-" + currentItemIndex);
-
-    newItem.classList.remove("unselected");
-    newItem.classList.add("selected");
-
-    //Gestisce il comportamento degli elementi a sx
-    document.querySelector(".showing-image").style.backgroundImage = "url(" + items[currentItemIndex] + ")";
-
-    document.querySelector(".showing-image-title").innerHTML = title[currentItemIndex];
-
-    document.querySelector(".showing-image-text").innerHTML = text[currentItemIndex];
 });
